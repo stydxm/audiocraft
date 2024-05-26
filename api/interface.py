22fce76f9b6f4786d8bbcc8a878ddb5cb17c9bbb
stydxm <stydxm@outlook.com>
2024-05-26 15:39:24 +0800

    update interface

--- api/interface.py
+++ api/interface.py
@@ -1,12 +1,8 @@
+import time
+
 import gradio as gr
 from gradio.themes import Base, colors, sizes, GoogleFont
-import time
-import os
 
-title = """<h1 align="center"><a href="https://github.com/OpenGVLab/Ask-Anything"><img src="https://s1.ax1x.com/2023/05/07/p9dBMOU.png" alt="Ask-Anything" border="0" style="margin: 0 auto; height: 100px;" /></a> </h1>"""
-description = """
-        VideoChat2 powered by InternVideo!<br><p><a href='https://github.com/OpenGVLab/Ask-Anything'><img src='https://img.shields.io/badge/Github-Code-blue'></a></p><p>
-        """
 gvlabtheme = Base(
     primary_hue=colors.blue,
     secondary_hue=colors.sky,
@@ -62,48 +58,82 @@ with gr.Blocks(
     # theme=gvlabtheme,
     # css="#chatbot {overflow:auto; height:500px;} #InputVideo {overflow:visible; height:320px;} footer {visibility: none}",
 ) as demo:
-
     with gr.Row():
         with gr.Column(scale=4, visible=True) as video_upload:
-            with gr.Column(elem_id="image") as img_part:
-                up_video = gr.Video(
-                    interactive=True, include_audio=True, elem_id="video_upload"
-                )
+            up_video = gr.Video(
+                interactive=True, include_audio=True, elem_id="video_upload"
+            )
             upload_button = gr.Button(
                 value="Upload", interactive=True, variant="primary"
             )
+            video_reset_button = gr.Button("Reset")
 
         with gr.Column(scale=6, visible=True) as perception_module:
-            with gr.Tab("overall", elem_id="overall"):
-                overall_caption = gr.Textbox(
-                    label="Overall Caption",
-                    value="Please upload your video first",
-                    interactive=False,
-                    lines=2,
-                )
-                chatbot = gr.Chatbot(elem_id="chatbot", label="Key Aspects")
-                # with gr.Row():
-                #     with gr.Column(scale=7):
-                #         text_input = gr.Textbox(
-                #             show_label=False,
-                #             placeholder="Please upload your video first",
-                #             interactive=False,
-                #         )
-                # with gr.Column(scale=3, min_width=0):
-                #     run = gr.Button("Send")
-            outputs = [overall_caption]
+            # with gr.Row():
+            #     with gr.Column(scale=7):
+            #         text_input = gr.Textbox(
+            #             show_label=False,
+            #             placeholder="Please upload your video first",
+            #             interactive=False,
+            #         )
+            # with gr.Column(scale=3, min_width=0):
+            #     run = gr.Button("Send")
+            text_outputs = []
             for i in range(1, 5):
                 tab = gr.Tab("scene" + str(i), elem_id="scene" + str(i))
                 tab.__enter__()
-                clip_perception = gr.Textbox(
-                    label="Clip Perception",
-                    value="Please upload your video first",
-                    interactive=False,
-                    lines=2,
-                )
-                chatbot = gr.Chatbot(elem_id="chatbot", label="Key Aspects")
-                outputs.append(clip_perception)
+                with gr.Row():
+                    clip_chatbot = gr.Chatbot(
+                        elem_id="chatbot",
+                        label="Discussions between Two Agents",
+                        height="10em",
+                    )
+                with gr.Row():
+                    with gr.Column():
+                        clip_caption = gr.Textbox(
+                            label="Clip Perception",
+                            value="Please upload your video first",
+                            interactive=False,
+                            lines=2
+                        )
+                        clip_emotion = gr.Textbox(label="Emotion",value="emotion", interactive=False,lines=2)
+                        clip_subtitles = gr.Textbox(
+                            label="Subtitles", value="subtitles",lines=2
+                        )
+                    with gr.Column():
+                        clip_aspects = gr.Textbox(
+                            label="Key Aspects", value="key aspects",lines=2,scale=2
+                        )
+                        clip_possible_sounds = gr.Audio(
+                            label="Possible Sounds", value="./api/generated.wav",scale=1
+                        )
+                text_outputs.append(clip_caption)
+                text_outputs.append(clip_emotion)
+                text_outputs.append(clip_subtitles)
+                text_outputs.append(clip_aspects)
                 tab.__exit__()
+
+            with gr.Tab("overall", elem_id="overall"):
+                with gr.Row():
+                    overall_chatbot = gr.Chatbot(
+                        elem_id="chatbot",
+                        label="Discussions between Two Agents",
+                        height="10em",
+                    )
+                with gr.Row():
+                    with gr.Column():
+                        overall_caption = gr.Textbox(
+                            label="Overall Caption",
+                            value="Please upload your video first",
+                            interactive=False,
+                            lines=2
+                        )
+                        overall_emotion = gr.Textbox(label="Emotion Variant",value="emotion variant", interactive=False,lines=2)
+                    with gr.Column():
+                        clip_aspects = gr.Textbox(label="Key Aspects", value="key aspects",lines=7)
+                with gr.Row():
+                    bgm_description=gr.Textbox(label="BGM Description", value="bgm description",lines=2)
+            text_outputs.append(overall_caption)
         with gr.Column(scale=4, visible=False) as audio_preview:
             musicgen = gr.Audio()
             audiogen = gr.Audio()
@@ -112,16 +142,16 @@ with gr.Blocks(
 
         with gr.Column(scale=6, visible=False) as result:
             video_result = gr.Video()
-
-    reset_button = gr.Button("Reset")
+            reset_button = gr.Button("Reset All")
 
     upload_button.click(
         lambda: (gr.update(visible=False), gr.update(visible=True)),
         None,
         [video_upload, audio_preview],
     )
-    upload_button.click(upload_video, outputs, outputs)
+    upload_button.click(upload_video, text_outputs, text_outputs)
     upload_button.click(mock_audio_generation, None, [musicgen, audiogen, tts])
+    video_reset_button.click(lambda: gr.Video(), None, [up_video])
     # generate_button.click(generate, None, [result])
     generate_button.click(
         lambda: (gr.update(visible=False), gr.update(visible=True)),
